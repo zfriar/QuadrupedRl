7ff3efe0b55add8df33e2ec2ee08b7f29672bf68
Zach Friar <zach.friar@gmail.com>
2025-11-30 16:07:41 -0800

    test(conftest): make sure each instances have fresh pybullet connections.

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,3 +1,4 @@
+import pybullet
 import pytest
 
 import envs.ant_env
@@ -5,6 +6,11 @@ import envs.ant_env
 
 @pytest.fixture
 def ant_environment():
+    """Provide an AntEnv instance for testing."""
+    # Ensure fresh connection context
+    if pybullet.isConnected():
+        pybullet.disconnect()
+
     env = envs.ant_env.AntEnv(gui=False)
     yield env
     env.close()
